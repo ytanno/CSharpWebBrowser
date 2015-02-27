5439a093ed9b7028fd76e5199460c4078682b521
y. tanno <s1130151@gmail.com>
2015-02-27 17:54:32 +0900

    2015/02/27

---
+++ browserMonitor/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace browserMonitor
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 必要なデザイナー変数です。
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 使用中のリソースをすべてクリーンアップします。
+		/// </summary>
+		/// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+		protected override void Dispose(bool disposing)
+		{
+			if ( disposing && ( components != null ) )
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows フォーム デザイナーで生成されたコード
+
+		/// <summary>
+		/// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+		/// コード エディターで変更しないでください。
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.panel1 = new System.Windows.Forms.Panel();
+			this.webBrowser1 = new System.Windows.Forms.WebBrowser();
+			this.button1 = new System.Windows.Forms.Button();
+			this.button2 = new System.Windows.Forms.Button();
+			this.panel1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// panel1
+			// 
+			this.panel1.Controls.Add(this.webBrowser1);
+			this.panel1.Location = new System.Drawing.Point(23, 57);
+			this.panel1.Name = "panel1";
+			this.panel1.Size = new System.Drawing.Size(869, 610);
+			this.panel1.TabIndex = 0;
+			// 
+			// webBrowser1
+			// 
+			this.webBrowser1.Dock = System.Windows.Forms.DockStyle.Fill;
+			this.webBrowser1.Location = new System.Drawing.Point(0, 0);
+			this.webBrowser1.MinimumSize = new System.Drawing.Size(20, 20);
+			this.webBrowser1.Name = "webBrowser1";
+			this.webBrowser1.Size = new System.Drawing.Size(869, 610);
+			this.webBrowser1.TabIndex = 0;
+			// 
+			// button1
+			// 
+			this.button1.Location = new System.Drawing.Point(23, 13);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 1;
+			this.button1.Text = "C#入力";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// button2
+			// 
+			this.button2.Location = new System.Drawing.Point(129, 13);
+			this.button2.Name = "button2";
+			this.button2.Size = new System.Drawing.Size(111, 23);
+			this.button2.TabIndex = 2;
+			this.button2.Text = "検索ボタンクリック";
+			this.button2.UseVisualStyleBackColor = true;
+			this.button2.Click += new System.EventHandler(this.button2_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(925, 679);
+			this.Controls.Add(this.button2);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.panel1);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.panel1.ResumeLayout(false);
+			this.ResumeLayout(false);
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.Panel panel1;
+		private System.Windows.Forms.WebBrowser webBrowser1;
+		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Button button2;
+
+	}
+}
+
---
+++ browserMonitor/Form1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace browserMonitor
+{
+	public partial class Form1: Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			webBrowser1.Url = new Uri(@"https://www.google.co.jp/");
+		}
+
+		private void button1_Click(object sender, EventArgs e)
+		{
+			var qInputs = webBrowser1.Document.All.GetElementsByName("q");
+			qInputs[0].InnerText = "C#";
+		}
+
+		private void button2_Click(object sender, EventArgs e)
+		{
+			var btns = webBrowser1.Document.All.GetElementsByName("btnG");
+			btns[0].InvokeMember("click");
+		}
+	}
+}
